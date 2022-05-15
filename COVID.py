91ecb1109ade972465d4c4575c6ddeeaaa99e5f9
KChaugule <88726136+KChaugule@users.noreply.github.com>
2022-05-15 16:04:14 -0400

    Update COVID.py

--- COVID.py
+++ COVID.py
@@ -1,5 +1,3 @@
-symptoms = ["fever or chills", "cough", "shortness of breath or difficulty breathing", "fatigue", "muscle or body aches", "headache", "new loss of taste or smell", "sore throat", "congestion or runny nose", "nausea or vomiting", "diarrhea"]
-percent = [11.69, 10.39, 12.34, 8.44, 7.79, 7.79, 12.34, 10.39, 7.14, 5.84, 5.84]
 for i in range(1,4):
     print("#", end="")
     for x in range(1,22):
@@ -11,6 +9,12 @@ for j in range(1,4):
     for k in range(1,22):
         print("*", end="")
     print("#")
-prob = 0
-know = input("Do you want to know the probability that you have COVID-19?\n")
-know = know.lower()
+from functions import calculation
+from functions import database
+choice = input("Choose a or b\na. Calculate the probability that you have Covid\nb. See past results\nc. Terminate program\n")
+choice = choice.lower()
+while choice == "a" or choice == "b":
+    if choice == "a":
+        calculation()
+    elif choice == "b":
+        database()
